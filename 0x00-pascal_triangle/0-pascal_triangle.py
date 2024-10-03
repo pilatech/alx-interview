6ead2934d5779450b472f4dc7427b1110939ea46
Pilate <pilatech6@gmail.com>
2024-10-03 17:35:01 +0200

    create semi-manual Pascal triangle up 5th row

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,4 +12,17 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    return [1, 2, 3]
+    pascal = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                if j == 1 or j == (i - 1):
+                    row.append(i)
+                else:
+                    row.append(6)
+
+        pascal.append(row)
+    return pascal
