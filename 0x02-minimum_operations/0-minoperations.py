496a6a339a35560388ef6619bf42e51532354ebc
Pilate <pilatech6@gmail.com>
2024-10-17 10:30:22 +0200

    min operations starter

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyton3
+"""Find out the minimum operations"""
+
+
+def minOperations(n):
+    """Implement the mimimum operations
+    Args:
+        n(number) - number of commands
+    Return:
+        (number) - minimum operations
+    """
+    if n == 0:
+        return 0
+    else:
+        return = 1 + minOperations(n - 1)
