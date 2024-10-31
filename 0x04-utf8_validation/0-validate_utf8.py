ce1cbd78cc81da111107c4c0a50e898c966586cf
Pilate <pilatech6@gmail.com>
2024-10-31 19:11:00 +0200

    initialize utf-8 validation project

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Implement utf-8 validation"""
+
+
+def validUTF8(data):
+    """Check if data is a valid utf8
+    Args:
+        data(any): the data to check
+    Return:
+        (boolean) whether it's a valid utf-8 or not
+    """
+    return True
