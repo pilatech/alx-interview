7fa7ea0b5897a2cc8f1a0c8898e24e21d08cb41b
Pilate <pilatech6@gmail.com>
2025-01-29 15:38:23 +0200

    add make-change algorithm challenge solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""File for implementing 'makeChange' function
+"""
+
+
+def getLargest(coins, total):
+    """get largest changeworthy coin
+    Args:
+        coins - (list) the coins to choose from
+        total - (int) the amount to find change for
+    """
+    largest = 0
+    for coin in coins:
+        if coin <= total and coin > largest:
+            largest = coin
+    return largest
+
+
+def makeChange(coins, total):
+    """Determine minimum number of coins for a change
+    Args:
+        coins - (list) the coins we have
+        total - (int) the amount we need change for
+    """
+    if total == 0:
+        return 0
+    change = []
+    while (total > 0):
+        largest = getLargest(coins, total)
+        if largest == 0:
+            break
+        change.append(largest)
+        total -= largest
+    if total > 0:
+        return -1
+    return len(set(change))
