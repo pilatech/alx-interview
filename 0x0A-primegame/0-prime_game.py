4eece83f21943fd0bda98ee2441384c447cc8acd
Pilate <pilatech6@gmail.com>
2025-02-11 10:08:04 +0200

    fix_1 prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,22 +2,6 @@
 """Module implementing Prime Game."""
 
 
-def is_prime(n):
-    """Check if number is prime"""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def primes_in_range(start, end):
-    """Get list of prime numbers in a range"""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
-
-
 def isWinner(x, nums):
     """Function for finding the winner
     """
@@ -56,3 +40,19 @@ def isWinner(x, nums):
         return "Winner: Ben"
 
     return None
+
+
+def is_prime(n):
+    """Check if number is prime"""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_in_range(start, end):
+    """Get list of prime numbers in a range"""
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
